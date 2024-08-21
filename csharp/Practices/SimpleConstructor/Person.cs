ad89c22c1412adb4859a612d7e1c15f197bea797
Luc Daigremont <luc.daigremont@philips.com>
2024-08-21 10:47:38 +0200

    [csharp] Add Correction for simple constructor and a second correction

--- csharp/Practices/SimpleConstructor/Person.cs
+++ csharp/Practices/SimpleConstructor/Person.cs
@@ -1,18 +1,19 @@
 ﻿namespace Practices.SimpleConstructor
 {
-    public class Person
-    {
-        public Title Title { get; private set; }
-        public string Name { get; private set; }
-        public int AgeAtCreation { get; private set; }
+    public record Person(string Name, int AgeAtCreation, Title Title);
 
-        public Person(string name, DateTime birthdate, int title)
+    public static class PersonConstructor
+    {
+        public static Person ConstructPerson(string name, DateTime birthdate, int title)
         {
-            Name = string.Concat(name[0].ToString().ToUpper(), name.AsSpan(1));
-            Title = (Title)title;
+
+            var capitalizedName = string.Concat(name[0].ToString().ToUpper(), name.AsSpan(1));
+            var titleenum = (Title)title;
             TimeSpan diff = (DateTime.Now - birthdate);
             DateTime zeroTime = new DateTime(1, 1, 1);
-            AgeAtCreation = (zeroTime + diff).Year - 1;
+            var ageAtCreation = (zeroTime + diff).Year - 1;
+
+            return new Person(capitalizedName, ageAtCreation, titleenum);
         }
     }
 }
---
+++ csharp/Practices/SimpleConstructor/Person2.cs
@@ -0,0 +1,66 @@
+﻿namespace Practices.SimpleConstructor
+{
+    public class PersonBuilder
+    {
+        private string? _capitalizedName;
+        private Title? _title;
+        private int? _ageAtCreation;
+
+        public PersonBuilder AddName(string name)
+        {
+            _capitalizedName = string.Concat(name[0].ToString().ToUpper(), name.AsSpan(1));
+
+            return this;
+        }
+
+        public PersonBuilder AddTitle(int title)
+        {
+            if (Enum.IsDefined(typeof(Title), title))
+            {
+                _title = (Title)title;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Title is not known");
+            }
+
+            return this;
+        }
+
+        public PersonBuilder AddBirthdate(DateTime birthdate)
+        {
+            TimeSpan diff = (DateTime.Now - birthdate);
+            DateTime zeroTime = new DateTime(1, 1, 1);
+            _ageAtCreation = (zeroTime + diff).Year - 1;
+
+            return this;
+        }
+
+        public Person? Build()
+        {
+            if (_capitalizedName == null)
+                throw new ArgumentNullException("Name is mandatory");
+            if (_title == null)
+                throw new ArgumentNullException("Title is mandatory");
+            if (_ageAtCreation == null)
+                throw new ArgumentNullException("Birthdate is mandatory");
+
+            return new Person(_capitalizedName, _ageAtCreation.Value, _title.Value);
+        }
+    }
+
+    public static class Example
+    {
+        public static void CallBuilder()
+        {
+            var builder = new PersonBuilder();
+
+            builder
+                .AddTitle(1)
+                .AddBirthdate(new DateTime(2021, 1, 1))
+                .AddName("name");
+
+            var person = builder.Build();
+        }
+    }
+}
