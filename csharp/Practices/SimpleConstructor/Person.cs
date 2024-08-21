3aa7d05ff820b4a1ffb776625f7e02ab17e4765f
Luc Daigremont <luc.daigremont@philips.com>
2024-08-21 10:27:11 +0200

    Add bad example for simple constructor

---
+++ csharp/Practices/SimpleConstructor/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Practices.SimpleConstructor
+{
+    public class Person
+    {
+        public Title Title { get; private set; }
+        public string Name { get; private set; }
+        public int AgeAtCreation { get; private set; }
+
+        public Person(string name, DateTime birthdate, int title)
+        {
+            Name = string.Concat(name[0].ToString().ToUpper(), name.AsSpan(1));
+            Title = (Title)title;
+            TimeSpan diff = (DateTime.Now - birthdate);
+            DateTime zeroTime = new DateTime(1, 1, 1);
+            AgeAtCreation = (zeroTime + diff).Year - 1;
+        }
+    }
+}
