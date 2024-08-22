eec4a87af2892b83b77e0313aafec504e7865a9e
Luc Daigremont <luc.daigremont@philips.com>
2024-08-22 13:58:15 +0200

    [csharp] Add Collections should not be null

---
+++ csharp/Practices/CollectionsNotNull/OrderProcessor.cs
@@ -0,0 +1,15 @@
+﻿namespace Practices.CollectionsNotNull
+{
+    public class OrderProcessor
+    {
+        private List<Order> _orders = new();
+
+        public void AddOrders(List<Order> orders)
+        {
+            if (orders == null)
+                return;
+
+            _orders.AddRange(orders);
+        }
+    }
+}
