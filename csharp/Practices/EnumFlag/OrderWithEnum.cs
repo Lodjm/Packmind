08dbe911ffe23188f331dfad77957aea4a53839f
Luc Daigremont <luc.daigremont@philips.com>
2024-08-26 10:56:17 +0200

    [csharp] Add EnumFlag bad example

---
+++ csharp/Practices/EnumFlag/OrderWithEnum.cs
@@ -0,0 +1,31 @@
+﻿namespace Practices.EnumFlag
+{
+    public enum MyBadEnum
+    {
+        One = 1,
+        Two = 2,
+        Four = 4
+    }
+
+    public interface IMultipleEnum<T>
+    {
+        void AddMyEnum(T myEnum);
+        bool Contains(T myEnum);
+    }
+
+    public class OrderWithEnum : IMultipleEnum<MyBadEnum>
+    {
+        public List<MyBadEnum> Enums { get; set; } = new();
+
+        public void AddMyEnum(MyBadEnum myBadEnum)
+        {
+            if (!Enums.Contains(myBadEnum))
+                Enums.Add(myBadEnum);
+        }
+
+        public bool Contains(MyBadEnum myBadEnum)
+        {
+            return Enums.Contains(myBadEnum);
+        }
+    }
+}
