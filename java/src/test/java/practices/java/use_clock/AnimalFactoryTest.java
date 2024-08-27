60730034c5960436e4838f38ef4a36a69ab3f538
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-08-27 10:27:07 +0200

    Initial commit for java

--- java/src/test/java/practices/java/use_clock/AnimalFactoryTest.java
+++ java/src/test/java/practices/java/use_clock/AnimalFactoryTest.java
@@ -10,6 +10,7 @@ class AnimalFactoryTest {
     @Test
     void init_birthday() {
         Animal actual = animalFactory.createAnimalNamed("toto");
-        assertThat(actual.birthday()).isEqualTo("2024-07-14T12:34:00.00Z");
+        // can't test date
+        // assertThat(actual.birthday()).isEqualTo("2024-07-14T12:34:00.00Z");
     }
 }
