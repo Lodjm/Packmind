115e19c5381d4b648c3197be69d045556cfa100b
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-08-27 10:32:19 +0200

    Initial commit for java

--- java/src/main/java/practices/java/use_clock/AnimalFactory.java
+++ java/src/main/java/practices/java/use_clock/AnimalFactory.java
@@ -1,11 +1,17 @@
 package practices.java.use_clock;
 
+import java.time.Clock;
 import java.time.Instant;
 
 public class AnimalFactory {
+    private final Clock clock;
+
+    public AnimalFactory(Clock clock) {
+        this.clock = clock;
+    }
 
     public Animal createAnimalNamed(String name) {
-        Instant birthday = Instant.now();
+        Instant birthday = clock.instant();
         return new Animal(name, birthday);
     }
 }
--- java/src/test/java/practices/java/use_clock/AnimalFactoryTest.java
+++ java/src/test/java/practices/java/use_clock/AnimalFactoryTest.java
@@ -2,10 +2,15 @@ package practices.java.use_clock;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AnimalFactoryTest {
-    AnimalFactory animalFactory = new AnimalFactory();
+    AnimalFactory animalFactory = new AnimalFactory(Clock.fixed(
+            Instant.parse("2024-07-14T12:34:00.00Z"), ZoneId.of("UTC")));
 
     @Test
     void init_birthday() {
