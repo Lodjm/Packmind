e2c9cd303590ffddf4aa3fdfd09f17d5fefd618f
Alexandre Duchenne <alexduch@hotmail.fr>
2024-08-28 17:01:41 +0200

    [Java] Add a correct code example using a relationship

--- java/src/main/java/practices/java/orm/no_entity_for_join_table/Author.java
+++ java/src/main/java/practices/java/orm/no_entity_for_join_table/Author.java
@@ -1,7 +1,12 @@
 package practices.java.orm.no_entity_for_join_table;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import java.util.List;
 
 @Entity
 class Author {
@@ -9,4 +14,12 @@ class Author {
   @Id
   String uid;
   String fullName;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+      name = "BOOK_AUTHORS",
+      joinColumns = @JoinColumn(name = "authorUid"),
+      inverseJoinColumns = @JoinColumn(name = "isbn")
+  )
+  List<Book> books;
 }
--- java/src/main/java/practices/java/orm/no_entity_for_join_table/Library.java
+++ java/src/main/java/practices/java/orm/no_entity_for_join_table/Library.java
@@ -7,16 +7,13 @@ import javax.transaction.Transactional;
 public class Library {
 
   AuthorsRepository authors;
-  BooksRepository books;
-  BookAuthorsRepository bookAuthors;
 
   @Transactional
   List<Book> search(String authorFullName) {
     List<Author> matchingAuthors = authors.findByFullName(authorFullName);
     List<Book> searchResults = new ArrayList<>();
     for(Author author : matchingAuthors) {
-      bookAuthors.findById_AuthorUid(author.uid)
-          .forEach(bookAuthor -> books.findById(bookAuthor.id.isbn).ifPresent(searchResults::add));
+      searchResults.addAll(author.books);
     }
     return searchResults;
   }
