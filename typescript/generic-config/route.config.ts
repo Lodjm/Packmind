db5c7333f4ae147cbf906097612baa236405116b
Maguran <maguran.mahendrarajan@gmail.com>
2024-08-30 21:13:02 +0200

    [Typescript] add fix for  generic config for route configuration

--- typescript/generic-config/route.config.ts
+++ typescript/generic-config/route.config.ts
@@ -2,10 +2,13 @@ const ROUTES_CONFIG = {
     default: '/',
     home: '/home',
     auth: '/auth'
-}
+} as const;
 
-const goToRoute = (route: '/' | '/home' | '/auth') => {
+// type RouteConfig = "/" | "/home" | "/auth"
+type RouteConfig = typeof ROUTES_CONFIG[keyof typeof ROUTES_CONFIG] 
+
+const goToRoute = (route: RouteConfig) => {
     // Handle your route navigation
 }
 
-goToRoute('/auth')
+goToRoute(ROUTES_CONFIG.auth)
