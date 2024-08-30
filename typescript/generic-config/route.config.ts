26ee25b038bcd9f7e661df304e3ff20b330d3a44
Maguran <maguran.mahendrarajan@gmail.com>
2024-08-30 21:12:00 +0200

    [Javascript] add bad pratice generic config for route configuration

---
+++ typescript/generic-config/route.config.ts
@@ -0,0 +1,11 @@
+const ROUTES_CONFIG = {
+    default: '/',
+    home: '/home',
+    auth: '/auth'
+}
+
+const goToRoute = (route: '/' | '/home' | '/auth') => {
+    // Handle your route navigation
+}
+
+goToRoute('/auth')
