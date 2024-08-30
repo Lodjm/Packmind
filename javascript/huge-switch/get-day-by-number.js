29e78e4be2c8ec57cdb35ddafed4b2e8d29c5f5b
Maguran <maguran.mahendrarajan@gmail.com>
2024-08-30 21:37:40 +0200

    [Javascript] Add fix for huge switch with getDayByNumber function

--- javascript/huge-switch/get-day-by-number.js
+++ javascript/huge-switch/get-day-by-number.js
@@ -1,20 +1,13 @@
 function getDayByNumber(day) {
-    switch (day) {
-        case 1:
-            return "Monday";
-        case 2:
-            return "Tuesday";
-        case 3:
-            return "Wednesday";
-        case 4:
-            return "Thursday";
-        case 5:
-            return "Friday";
-        case 6:
-            return "Saturday";
-        case 7:
-            return "Sunday";
-        default:
-            throw new Error("day must be in range 1 to 7");
-    }
+    if (day < 1 || day > 7) throw new Error("day must be in range 1 to 7");
+    const days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ];
+    return days[day - 1];
 }
