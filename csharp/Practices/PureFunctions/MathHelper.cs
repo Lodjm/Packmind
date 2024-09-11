37c5fb3edb9fb9d629f46c98c8590f6281790ebc
Luc Daigremont <luc.daigremont@gmail.com>
2024-09-11 10:48:31 +0200

    [csharp] Add bad example for use pure functions

---
+++ csharp/Practices/PureFunctions/MathHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Practices.PureFunctions
+{
+    public static class MathHelper
+    {
+        public static int Sum(int x, int y, ILogger logger)
+        {
+            logger.Log(LogLevel.Information, $"Sum {x} + {y}");
+            return x + y;
+        }
+    }
+
+    public class Example
+    {
+        private readonly ILogger<Example> _logger;
+
+        public Example(ILogger<Example> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Process()
+        {
+            var r = MathHelper.Sum(1, 1, _logger);
+        }
+    }
+}
